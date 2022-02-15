6254e1f73f58be9bf77d5adc45d86149f4017803
Eoinburke <burkeyeoin45@gmail.com>
2022-02-15 20:04:17 +0000

    Added error html and css pages

--- env.py
+++ env.py
@@ -1,3 +1,4 @@
 import os
 
 os.environ['SECRET_KEY'] = 'sk_test_51K0vkpCeOcKJ8dALxAkt5YnnWi5l9AetUJm55DDHL6NciOigqViIRTDYOwldLHmcHiRFodEB0LnTvXtf83wERMpM000f1CaoIo'
+os.environ['PUBLIC_KEY'] = 'pk_test_51K0vkpCeOcKJ8dALxoS9gzago2Bvr5QKZlOFvRtGz9N8obYG5BGChrO6q0sM4uDFv30bL9Ce5PJvF7WiCBnBiqzE00i7VrpgvY'
--- mls4/settings.py
+++ mls4/settings.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 
-
+if os.path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -184,7 +185,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
-STRIPE_CURRENCY = 'usd'
+STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
