408ecdd69e399e21395ff70c18bf375f325379cc
Eoinburke <burkeyeoin45@gmail.com>
2022-02-16 21:37:42 +0000

    Added favicon change mobile hamburger menu

--- bag/views.py
+++ bag/views.py
@@ -39,6 +39,7 @@ def add_to_bag(request, item_id):
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
+            
 
     request.session['bag'] = bag
     return redirect(redirect_url)
