95d8fb506ced6bca620bd28872e912c1e9228c5a
Eoinburke <burkeyeoin45@gmail.com>
2022-02-19 21:21:45 +0000

    Removed secret key and set debug

--- mls4/settings.py
+++ mls4/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z-5#=mu-_8(qulo(d400kqbn=_9j8%p$mn)k*z!)0lp5o)_mgl'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 
 ALLOWED_HOSTS = ['mls4-eb.herokuapp.com', 'localhost']
